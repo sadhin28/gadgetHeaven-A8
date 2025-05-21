e02d967bc03a38657256d46911d3291f8102195d
sadhin28 <taosifsadhin113@gmail.com>
2025-05-22 05:14:15 +0600

    add cad to use loader

---
+++ src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Card = ({product}) => {
+    console.log()
+    return (
+        <div>
+            <h1>Product Name:{product.name} </h1>
+        </div>
+    );
+};
+
+export default Card;
--- src/Components/GadgetContainer.jsx
+++ src/Components/GadgetContainer.jsx
@@ -1,14 +1,17 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import Card from "./Card";
 
 const GadgetContainer = () => {
      const obj=useParams();
      const gadets = useLoaderData();
      
-     const  gadegetData = gadets.filter(data=>data.category === obj.category)
+     const  gadegetData = gadets.filter(data=>data.category.includes(obj.category))
      console.log(gadegetData)
     return (
         <div>
-            <h1>hi</h1>
+            {
+                gadegetData.map(product=><Card key={product.id} product={product}></Card>)
+            }
         </div>
     );
 };
