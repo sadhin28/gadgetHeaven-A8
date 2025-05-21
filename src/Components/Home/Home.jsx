d611b583c3144a2975f0d0dbef08b81a4d5412ea
sadhin28 <taosifsadhin113@gmail.com>
2025-05-22 01:54:11 +0600

    add hero section data

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
            <div className='mb-10 bg-[#9538E2] h-[510px] md:h-[610px]' >
                
            </div>
-            <div id='banner' className='w-9/12 mx-auto md:h-120 h-50  relative bottom-35 md:bottom-70'>
-                <img className='w-full h-full' src={image} alt="" />
+            <div id='banner' className=' w-9/12 mx-auto md:h-120 h-50  relative bottom-35 md:bottom-70'>
+                <img className='w-full rounded-2xl h-full' src={image} alt="" />
             </div>
             <div className='flex gap-10 '>
                     <div className=' h-70 border w-3/12'>
