fb709b180078a4d3baa847136d3386badb1891d4
sadhin28 <taosifsadhin113@gmail.com>
2025-05-26 18:47:25 +0600

    add wish list

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -29,8 +29,8 @@ const Dashboard = () => {
                 <h1 className="text-center text-2xl font-bold ">Dashboard</h1>
                 <p className="text-center">Explore the latest gadgets that will take your <br /> experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
                 <div className="flex  py-5 justify-center gap-10 items-center" >
-                    <NavLink onClick={() => handleisActive('cart')} to="card" className="p-2 ttcard w-20 text-center border rounded-xl">Cart</NavLink>
-                    <NavLink onClick={() => handleisActive('wishlists')} to="wishes" className="p-2 w-20 text-center border rounded-xl">WishList</NavLink>
+                    <NavLink onClick={() => handleisActive('cart')} to="card" className="p-2 ttcard w-30 text-center border rounded-xl">Cart</NavLink>
+                    <NavLink onClick={() => handleisActive('wishlists')} to="wishes" className="p-2 w-30 text-center border rounded-xl">WishList</NavLink>
                 </div>
             </div>
                 <div className=" p-2 md:text-2xl ">
