c25ae4d6bf69e2d8b0ac3e80bd75e1aa1a2297d2
sadhin28 <taosifsadhin113@gmail.com>
2025-05-26 19:25:48 +0600

    add all data

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="w-11/12 mx-auto">
         <Navbar data={data}></Navbar>
-       <div className="min-h-[calc(100vh-100px)]">
+       <div className="min-h-[calc(100vh-20px)]">
           <Outlet></Outlet>
        </div>
        <Footer></Footer>
