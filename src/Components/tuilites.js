275becc6a110f5e31ab9d6438a7e0ed954bfd5fd
sadhin28 <taosifsadhin113@gmail.com>
2025-05-26 17:23:40 +0600

    add wish list

--- src/Components/tuilites.js
+++ src/Components/tuilites.js
@@ -48,5 +48,30 @@ const  counts =(count,setcount)=>{
 }
 
 
+//wish list
+const getStoredwhishesList =()=>{
+    const storedListr=localStorage.getItem('whish-list')
+    if(storedListr){
+        const storedList = JSON.parse(storedListr);
+        return storedList;
+    }
+    else{
+        return [];
+    }
+}
 
-export {addToStoredReadList,getStoredReadList,removeFromLs}
+const addToStoredwishesList=(id)=>{
+    const storedList =  getStoredwhishesList();
+    if(storedList.includes(id)){
+
+        toast.error(`This Book alrady exists in the wishList`)
+    }
+    else{
+        storedList.push(id);
+        const storedListstr = JSON.stringify(storedList);
+        localStorage.setItem('whish-list',storedListstr)
+        toast.success('Wishes The Book')
+    }
+}
+
+export {addToStoredReadList,getStoredReadList,removeFromLs,addToStoredwishesList,getStoredwhishesList}
