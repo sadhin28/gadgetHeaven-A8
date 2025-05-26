7f6e44a39949d5eff9ac383aab3bf2a248ff5f11
sadhin28 <taosifsadhin113@gmail.com>
2025-05-26 18:30:29 +0600

    add wish card

--- src/Components/Nav/NavBar.jsx
+++ src/Components/Nav/NavBar.jsx
@@ -3,17 +3,25 @@ import { HiOutlineShoppingCart } from "react-icons/hi";
 import { AiOutlineHeart } from "react-icons/ai";
 import '../Nav/Navbar.css'
 import { getStoredwhishesList } from "../tuilites";
+import { useEffect, useState } from "react";
 
 const Navbar = ({data}) => {
-    const wishdata=getStoredwhishesList()
-    console.log(wishdata)
+        const wishdata =getStoredwhishesList()
+    const [wish,setwish]=useState(0)
+    console.log(wish)
+    useEffect(()=>{
+
+    setwish(wishdata)
+    
+    },[])
+
     const links =<>
          <NavLink to="/">Home</NavLink>
          <NavLink to="/Statistics">Statistics</NavLink>
          <NavLink to="/Dashboard">Dashboard</NavLink>
     </>
     return (
-        <div className="sticky  rounded-t-2xl shadow-sm h-20 top-0 z-20 navbar text-white bg-[#9538E2] duration-700  transform-border scale-3d  ">
+        <div className="sticky  rounded-b-2xl shadow-sm h-20 top-0 z-20 navbar text-white bg-[#9538E2] duration-700  transform-border scale-3d  ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -45,6 +53,9 @@ const Navbar = ({data}) => {
                             <HiOutlineShoppingCart />
                            
                         </div>
+                        <div className="relative text-center   left-13 bottom-5">
+                             {wish.length}
+                         </div>
                         <div className="p-2 bg-amber-50 rounded-full">
                             <AiOutlineHeart />
                             
