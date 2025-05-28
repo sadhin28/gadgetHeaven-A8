96bc9e7527f5015abd275ee139aed4d6aebe1ebd
sadhin28 <taosifsadhin113@gmail.com>
2025-05-29 02:05:10 +0600

    add barchart

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -6,15 +6,19 @@ const Statistics = () => {
     const data = useLoaderData()
     console.log(data)
     return (
-      <ResponsiveContainer width="100%" height="100%">
-          <BarChart   width={500}
+      <div className='mt-30'>
+         <ResponsiveContainer width="100%" height={400}>
+ <BarChart   width={500}
           height={400}
           data={data}>
                <XAxis dataKey='name'></XAxis>
                <YAxis dataKey=""></YAxis>
                <Bar dataKey='price'></Bar>
           </BarChart>
-      </ResponsiveContainer>
+         </ResponsiveContainer>
+         
+      
+      </div>
     );
 };
 
