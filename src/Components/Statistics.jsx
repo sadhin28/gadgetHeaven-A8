b2abcb29ff33b200c16e9a70b79db35392bcc81d
sadhin28 <taosifsadhin113@gmail.com>
2025-05-29 01:22:58 +0600

    add bar chart

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,45 +1,37 @@
 import React, { PureComponent } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
+
 import {
-  ComposedChart,
-  Line,
-  Area,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  Scatter,
-  ResponsiveContainer,
+    ComposedChart,
+    Line,
+    Area,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Scatter,
+    ResponsiveContainer,
+    BarChart,
+    LineChart,
 } from 'recharts';
+
 const Statistics = () => {
+
     const data = useLoaderData()
-    console.log(data)
+
     return (
-        <ResponsiveContainer width="100%" height="100%">
-        <ComposedChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-        >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="name" scale="band" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="price" fill="#8884d8" stroke="#8884d8" />
-          <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-          <Scatter dataKey="cnt" fill="red" />
-        </ComposedChart>
-      </ResponsiveContainer>
+        
+     <ResponsiveContainer width='100%' height='100%'>
+        <BarChart  data={data}>
+            <XAxis dataKey="name"></XAxis>
+             <YAxis dataKey=""/>
+               
+        </BarChart>
+     </ResponsiveContainer>
+      
+
     );
 };
 
